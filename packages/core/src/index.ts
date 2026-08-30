c59cafe726ca56e2f8cb412747dc62fa805cc2d5
giuliastro <giuliaccio@gmail.com>
2026-08-30 17:05:17 +0200

    core: export context-cost domain

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,6 +8,7 @@
 
 export * from './domain/actions.js';
 export * from './domain/budget.js';
+export * from './domain/context-cost.js';
 export * from './domain/action-conflicts.js';
 export * from './domain/capabilities.js';
 export * from './domain/compatibility-rows.js';
