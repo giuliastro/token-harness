a99ccf106aa849b89a97ccb5b772a55ab20b92ae
giuliastro <giuliaccio@gmail.com>
2026-09-17 09:35:20 +0200

    feat: promote reviewed managed optimizer providers

--- packages/core/src/metrics/optimization-stack.ts
+++ packages/core/src/metrics/optimization-stack.ts
@@ -331,9 +331,7 @@ function combinationReview(
   };
 }
 
-function componentDescriptors(
-  input: BuildOptimizationStackInput,
-): OptimizationComponentDescriptor[] {
+function componentDescriptors(input: BuildOptimizationStackInput): OptimizationComponentDescriptor[] {
   const descriptors = input.components.map((component) => ({ ...component }));
   const listed = new Set(descriptors.map((component) => component.providerId));
 
